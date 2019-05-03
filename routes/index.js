8dc0e79fe9fb72dcb60f1f7c922f8f77262e86bf
Wesley Berry <wesleyberry52@gmail.com>
2019-05-03 00:57:06 -0500

    debugggggggggg

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ const apis = require("./api");
 router.use("/api", apis);
 
 router.use(function(req, res) {
-    res.sendFile(path.join(__dirname, "../client/public/index.html"));
+    res.sendFile(path.join(__dirname, "../client/build/index.html"));
 });
 
 module.exports = router;
