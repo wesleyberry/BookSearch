0d7de72b8f5625de98d66a02c8e9c3ccc8382c4c
Wesley Berry <wesleyberry52@gmail.com>
2019-05-03 00:36:24 -0500

    debugging

--- server.js
+++ server.js
@@ -15,9 +15,14 @@ if (process.env.NODE_ENV === "production") {
     app.use(express.static("client/build"));
 }
 
+
 app.use("/api", apiRoutes);
 app.use("/api", routes);
+app.use(routes);
 
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebook");
 
